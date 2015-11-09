7206b9569de7cd34eb5bc8761a1236324f3507e3
Carlos Ballena <cballenar@gmail.com>
2015-11-09 18:22:25 -0500

    Updated scripts to handle errors

--- database-backup.sh
+++ database-backup.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
+DROPBOX_UPLOADER=[/path/to/dropbox_uploader.sh]
+CONFIG_FILE=[/path/to/.dropbox_uploader]
+
 DATE=$(date +"%Y%m%d%H%M")
 FILE="[NAME]_$DATE.sql.gz"
 LOCALPATH="/[LOCAL_DIRECTORY]/$FILE"
 REMOTEPATH="/[REMOTE_DIRECTORY]/$FILE"
 
-DROPBOX_UPLOADER=[/path/to/dropbox_uploader.sh]
-CONFIG_FILE=[/path/to/.dropbox_uploader]
+function clean_up
+{
+    # Perform program exit housekeeping
+    echo "Removing temporary files..."
+    rm -fr $LOCALPATH
+    echo "All done."
+    exit $1
+}
 
+# backup mysql
 echo "Creating backup..."
 mysqldump -u [MYSQL_USER] --password=[PASSWORD] [DATABASE] | gzip -9 > $LOCALPATH
+# check if backup was successfull
+if [ ${PIPESTATUS[0]} -ne "0" ]; then
+    echo "Failed to create backup. Aborting..."
+    clean_up 1
+fi
 
+# upload to dropbox
 echo "Uploading to Dropbox..."
-$DROPBOX_UPLOADER -f $CONFIG_FILE upload $LOCALPATH $REMOTEPATH; then
+if $DROPBOX_UPLOADER -f $CONFIG_FILE upload $LOCALPATH $REMOTEPATH; then
+    echo "Backup $FILE has been successfully uploaded to Dropbox"
+else
+    echo "Failed to upload backup. Aborting..."
+fi
 
-echo "Backup $FILE has been successfully uploaded to Dropbox"
-rm -fr $LOCALPATH
+clean_up
--- directory-backup.sh
+++ directory-backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DROPBOX_UPLOADER=[/path/to/dropbox_uploader.sh]
+CONFIG_FILE=[/path/to/.dropbox_uploader]
+
 DATE=$(date +"%Y%m%d%H%M")
 FILE="[NAME]_$DATE.tar.gz"
 LOCALPATH="/[LOCAL_DIRECTORY]/$FILE"
@@ -7,14 +10,31 @@ REMOTEPATH="/[REMOTE_DIRECTORY]/$FILE"
 
 BKP_DIRS="[/path/to/dir_1 /path/to/dir_2 /path/to/dir_3]"
 
-DROPBOX_UPLOADER=[/path/to/dropbox_uploader.sh]
-CONFIG_FILE=[/path/to/.dropbox_uploader]
+function clean_up
+{
+    # Perform program exit housekeeping
+    echo "Removing temporary files..."
+    rm -fr $LOCALPATH
+    echo "All done."
+    exit $1
+}
 
+# backup directories
 echo "Creating backup..."
 tar cf - $BKP_DIRS | gzip -9 > $LOCALPATH
+# check if backup was successfull
+if [ ${PIPESTATUS[0]} -ne "0" ] && [ ${PEPESTATUS[1]} -ne "0" ]; then
+    echo "Failed to create backup. Aborting..."
+    clean_up 1
+fi
 
+
+# upload to dropbox
 echo "Uploading to Dropbox..."
-$DROPBOX_UPLOADER -f $CONFIG_FILE upload "$BKP_FILE.gz" /
+if $DROPBOX_UPLOADER -f $CONFIG_FILE upload $LOCALPATH $REMOTEPATH; then
+    echo "Backup $FILE has been successfully uploaded to Dropbox"
+else
+    echo "Failed to upload backup. Aborting..."
+fi
 
-echo "Backup $FILE has been successfully uploaded to Dropbox"
-rm -fr $LOCALPATH
+clean_up
